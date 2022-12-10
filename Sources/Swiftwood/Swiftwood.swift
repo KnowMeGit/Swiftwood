16f0c043ebb931a38c4a38e8ce324cb653d31d46
Michael Redig <mredig@gmail.com>
2022-12-10 01:44:04 -0600

    (refactor) move swiftwood destination to another file

--- Sources/Swiftwood/Swiftwood.swift
+++ Sources/Swiftwood/Swiftwood.swift
@@ -1,13 +1,5 @@
 import Foundation
 
-public protocol SwiftwoodDestination: AnyObject {
-	var format: Swiftwood.Format { get }
-	var minimumLogLevel: Swiftwood.Level { get }
-	var logFilter: LogCategory.Filter { get set }
-	var shouldCensor: Bool { get set }
-	func sendToDestination(_ entry: Swiftwood.LogEntry)
-}
-
 // log dumping
 public class Swiftwood {
 	public static let logFileURLs: [URL] = []
